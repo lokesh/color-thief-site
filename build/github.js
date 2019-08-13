75dc6de628b4d47ff848e7cf74b2a57ebc6930e2
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2019-08-12 22:53:34 -0700

    add: Github star count button

---
+++ build/github.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const fetch = require("node-fetch");
+
+fetch(`https://api.github.com/repos/lokesh/color-thief`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(result) {
+      const data = JSON.stringify(result);
+      fs.writeFileSync('../js/github.json', data);
+    });
+
