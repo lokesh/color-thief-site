f25037782c4e16a2fa6812fd733e57823d6d4ad1
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2020-10-26 22:35:23 -0700

    build: Deploy to dreamhost with sftp

--- deploy.js
+++ deploy.js
@@ -1,30 +1,35 @@
 const path = require("path");
 const host = require("./.host.json");
-const FtpDeploy = require("ftp-deploy"); require
-const ftpDeploy = new FtpDeploy();
+const SFTPUpload = require('sftp-upload');
 
-var distDir = path.normalize(__dirname + "/dist/");
+const distDir = path.normalize(__dirname + "/dist/");
 
-var config = {
-    user: host.username,
-    password: host.password,
+const config = {
     host: host.host,
+    username: host.username,
+    password: host.password,
     port: host.port,
-    localRoot: distDir,
-    remoteRoot: host.directory,
+    path: distDir,
+    remoteDir: host.directory,
 
     // include: ['*', '**/*'],      // this would upload everything except dot files
-    include: ["**/*"],
+    // include: ["**/*"],
 
     // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
     // exclude: [".*", "package*", "*.md", "dist/**/*.map", "node_modules/**", "build/**"],
 
     // delete ALL existing files at destination before uploading, if true
-    deleteRemote: true,
-
+    // deleteRemote: true,
 };
 
-ftpDeploy
-    .deploy(config)
-    .then(res => console.log("finished:", res))
-    .catch(err => console.log(err));
+sftp = new SFTPUpload(config);
+    sftp.on('error', function(err) {
+        throw err;
+    })
+    .on('uploading', function(progress) {
+        console.log(`${progress.percent}% ${progress.file}`);
+    })
+    .on('completed', function() {
+        console.log('Upload Completed');
+    })
+    .upload();
